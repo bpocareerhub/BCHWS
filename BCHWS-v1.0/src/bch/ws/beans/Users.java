10b59e05464e6f26b316e694e838f82aa4cd82d9
Levi Bautsita <levi.bautista@bpocareerhub.com>
2012-09-05 15:04:58 +0800

    Update

--- BCHWS-v1.0/src/bch/ws/beans/Users.java
+++ BCHWS-v1.0/src/bch/ws/beans/Users.java
@@ -2,8 +2,11 @@ package bch.ws.beans;
 
 public class Users {
 	private long user_id;
-	private int group_id;
-	private String email, password;
+	private int group_id,parent_user_id,account_type_id,source_id;
+	private String email, password,alternate_email,firstname,middlename,lastname,address_details;
+	private String date_of_birth,profile_picture,phone_number,mobile_number;
+	private char gender_code,nationality_code,address_city_code,address_region_code,address_country_code,marital_status_code;
+	
 	
 	public Users() {
 		
@@ -22,6 +25,38 @@ public class Users {
 		this.group_id = group_id;
 		this.email = email;
 		this.password = password;
+	}
+	
+	public Users(long user_id, int group_id, int parent_user_id,
+			int account_type_id, int source_id, String email, String password,
+			String alternate_email, String firstname, String middlename,
+			String lastname, String address_details, String date_of_birth,
+			String profile_picture, String phone_number, String mobile_number,
+			char gender_code, char nationality_code, char address_city_code,
+			char address_region_code, char address_country_code,
+			char marital_status_code) {
+		this.user_id = user_id;
+		this.group_id = group_id;
+		this.parent_user_id = parent_user_id;
+		this.account_type_id = account_type_id;
+		this.source_id = source_id;
+		this.email = email;
+		this.password = password;
+		this.alternate_email = alternate_email;
+		this.firstname = firstname;
+		this.middlename = middlename;
+		this.lastname = lastname;
+		this.address_details = address_details;
+		this.date_of_birth = date_of_birth;
+		this.profile_picture = profile_picture;
+		this.phone_number = phone_number;
+		this.mobile_number = mobile_number;
+		this.gender_code = gender_code;
+		this.nationality_code = nationality_code;
+		this.address_city_code = address_city_code;
+		this.address_region_code = address_region_code;
+		this.address_country_code = address_country_code;
+		this.marital_status_code = marital_status_code;
 	}
 	public long getUser_id() {
 		return user_id;
@@ -46,6 +81,114 @@ public class Users {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public int getParent_user_id() {
+		return parent_user_id;
+	}
+	public void setParent_user_id(int parent_user_id) {
+		this.parent_user_id = parent_user_id;
+	}
+	public int getAccount_type_id() {
+		return account_type_id;
+	}
+	public void setAccount_type_id(int account_type_id) {
+		this.account_type_id = account_type_id;
+	}
+	public String getAlternate_email() {
+		return alternate_email;
+	}
+	public void setAlternate_email(String alternate_email) {
+		this.alternate_email = alternate_email;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getMiddlename() {
+		return middlename;
+	}
+	public void setMiddlename(String middlename) {
+		this.middlename = middlename;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getAddress_details() {
+		return address_details;
+	}
+	public void setAddress_details(String address_details) {
+		this.address_details = address_details;
+	}
+	public String getDate_of_birth() {
+		return date_of_birth;
+	}
+	public void setDate_of_birth(String date_of_birth) {
+		this.date_of_birth = date_of_birth;
+	}
+	public String getProfile_picture() {
+		return profile_picture;
+	}
+	public void setProfile_picture(String profile_picture) {
+		this.profile_picture = profile_picture;
+	}
+	public String getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+	public String getMobile_number() {
+		return mobile_number;
+	}
+	public void setMobile_number(String mobile_number) {
+		this.mobile_number = mobile_number;
+	}
+	public char getGender_code() {
+		return gender_code;
+	}
+	public void setGender_code(char gender_code) {
+		this.gender_code = gender_code;
+	}
+	public char getNationality_code() {
+		return nationality_code;
+	}
+	public void setNationality_code(char nationality_code) {
+		this.nationality_code = nationality_code;
+	}
+	public char getAddress_city_code() {
+		return address_city_code;
+	}
+	public void setAddress_city_code(char address_city_code) {
+		this.address_city_code = address_city_code;
+	}
+	public char getAddress_region_code() {
+		return address_region_code;
+	}
+	public void setAddress_region_code(char address_region_code) {
+		this.address_region_code = address_region_code;
+	}
+	public char getAddress_country_code() {
+		return address_country_code;
+	}
+	public void setAddress_country_code(char address_country_code) {
+		this.address_country_code = address_country_code;
+	}
+	public char getMarital_status_code() {
+		return marital_status_code;
+	}
+	public void setMarital_status_code(char marital_status_code) {
+		this.marital_status_code = marital_status_code;
+	}
+	public int getSource_id() {
+		return source_id;
+	}
+	public void setSource_id(int source_id) {
+		this.source_id = source_id;
 	}
 	@Override
 	public int hashCode() {
