baf4f025051039756c9c75f51e3d8c555e094cf6
llbautista <llbautista@BCH-D002.bpocareerhub.local>
2012-09-10 11:21:02 +0800

    update crud interface

--- BCHWS-v1.0/src/bch/ws/interfaces/CRUD.java
+++ BCHWS-v1.0/src/bch/ws/interfaces/CRUD.java
@@ -2,8 +2,6 @@ package bch.ws.interfaces;
 
 import java.util.ArrayList;
 
-import bch.ws.beans.Users;
-
 public interface CRUD {
 	public ArrayList<?> retrieveAll();
 	public Object retrieveById(int id);
