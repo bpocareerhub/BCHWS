05ed9d58aa03ee27ebd9816315a51121efc8cf7e
sparungao <sonny.parungao@bpocareerhub.com>
2012-09-11 18:21:46 +0800

    still working on Employer Staff Dao....

--- BCHWS-v1.0/src/bch/ws/dao/EmployerStaffDao.java
+++ BCHWS-v1.0/src/bch/ws/dao/EmployerStaffDao.java
@@ -12,20 +12,24 @@ import bch.ws.interfaces.DataAccessObject;
 public class EmployerStaffDao extends DataAccessObject implements CRUD {
 
 	@Override
-	public ArrayList<?> retrieveAll() {
+	public ArrayList<Users> retrieveAll() {
 		// TODO Auto-generated method stub
 		ArrayList<Users> employerStaff = new ArrayList<Users>();
 		
-		String sql = "select * from users where groud_id=3";
+		String sql = "select * from users where group_id=3";
 		ResultSet rs = this.getResultSet(sql);
 		
 		try  {
 			while(rs.next()) {
 				Users user = new Users();
-				
+				user.setUser_id(rs.getLong("user_id"));
+				user.setGroup_id(rs.getInt("group_id"));
+				user.setEmail(rs.getString("email"));
+				employerStaff.add(user);
 			}
 		} catch (SQLException e) {
-			
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 		return employerStaff;
@@ -33,8 +37,30 @@ public class EmployerStaffDao extends DataAccessObject implements CRUD {
 
 	@Override
 	public Object retrieveById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Users user = null;
+		
+		try {
+			String sql = "SELECT u.user_id,u.group_id,u.parent_user_id,u.account_type_id,u.email,u.password,up.source_id,up.firstname, up.middlename, up.lastname, up.alternate_email, unix_timestamp(u.date_created) as date_created, up.profile_picture,";
+			       sql += " unix_timestamp(up.date_of_birth) as date_of_birth, up.date_modified, up.gender_code, up.nationality_code, up.phone_number, up.mobile_number, up.address_details, up.address_city_code, up.address_region_code, up.address_country_code, up.marital_status_code";
+				   sql += " FROM `user_profile` AS up";
+				   sql += " INNER JOIN `users` AS u ON up.user_id = u.user_id"; 
+				   sql += " WHERE u.user_id=" + id;
+			ResultSet  rs = this.getResultSet(sql);
+			
+			if(rs != null) {
+				user = new Users();
+				user.setUser_id(rs.getLong("user_id"));
+				user.setFirstname(rs.getString("firstname"));
+				user.setMiddlename(rs.getString("middlename"));
+				user.setLastname(rs.getString("lastname"));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+			  
+		return user;
 	}
 
 	@Override
@@ -48,6 +74,45 @@ public class EmployerStaffDao extends DataAccessObject implements CRUD {
 		// TODO Auto-generated method stub
 		return false;
 	}
+	
+	
+	public ArrayList<Users> retrieveAllPurchasedProfileByStaffId(int staffId) {
+		
+		ArrayList<Users> purchasedProfiles = new ArrayList<Users>();
+		
+		String sql = "select up.user_id, if(epp.purchase_id is not null, 1, 0) as purchased, ups.industry_sector_id, ups.years_of_experience_id,";
+			sql += " up.lastname, up.firstname, up.middlename, ups.expected_salary_currency_id, ups.expected_salary,";
+			sql += " unix_timestamp(up.date_of_birth) as date_of_birth, educ.academic_id, upd.resume_filename, if(cap.application_id is not null, 1, 0) as applied";
+			sql += " from user_profile as up";
+			sql += " inner join employer_purchased_profiles as epp on up.user_id = epp.profile_user_id";
+			sql += " left join user_profile_summary as ups on up.user_id = ups.user_id";
+			sql += " left join user_profile_detail upd on up.user_id = upd.user_id";
+			sql += " left join career_applications as cap  on cap.career_id=up.user_id";
+			sql += " left join user_educational_backgrounds as educ on educ.user_id = ups.user_id";
+			sql += " WHERE epp.purchased_by_user_id=" + staffId + " AND up.user_id!=0 GROUP BY up.user_id";		
+		ResultSet  rs = this.getResultSet(sql);
+				
+		try  {
+			while(rs.next()) {
+				Users user = new Users();
+				user.setUser_id(rs.getLong("user_id"));
+				user.setFirstname(rs.getString("firstname"));
+				user.setMiddlename(rs.getString("middlename"));
+				user.setLastname(rs.getString("lastname"));
+				purchasedProfiles.add(user);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 
+		return purchasedProfiles;	
+	}
+	
+	
+	public ArrayList<?> retreiveAllCareerPost(int id) {
+		return null;
+	}
+	
 
 }
