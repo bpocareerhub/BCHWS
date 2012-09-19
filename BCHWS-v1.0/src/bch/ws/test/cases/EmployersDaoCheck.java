19ca497b3faf6851eae59d842cbb3bea7f497247
Ryan Bartolay <ryan.bartolay@bpocareerhub.com>
2012-09-19 19:15:12 +0800

    renamed project as Core

---
+++ BCHWS-v1.0/src/bch/ws/test/cases/EmployersDaoCheck.java
@@ -0,0 +1,15 @@
+package bch.ws.test.cases;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class EmployersDaoCheck {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
